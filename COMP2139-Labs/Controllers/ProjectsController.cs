07df8f8622b09bbb3e5697a1e973090ccace8593
Adam Simcoe <101442161@georgebrown.ca>
2024-01-26 22:23:00 -0500

    Initial project setup

---
+++ COMP2139-Labs/Controllers/ProjectsController.cs
@@ -0,0 +1,38 @@
+﻿using COMP2139_Labs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2139_Labs.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var projects = new List<Project>()
+            {
+                new Project { ProjectId = 1, Name = "Project 1", Description = "This is my first project" }
+            };
+
+            return View(projects);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+		[HttpPost]
+		public IActionResult Create(Project project)
+		{
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+        public IActionResult Details(int id) 
+        {
+            var project = new Project { ProjectId = id, Name = "Project " + id, Description = "Details of project " + id };
+            return View(project);
+        }
+    }
+}
